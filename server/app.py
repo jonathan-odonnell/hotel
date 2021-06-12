c086c4f54bb4174e7f7bf02cb3a304513ecc1161
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-12 18:37:30 +0100

    Amended database settings

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URL')
 db = SQLAlchemy(app)
 
 if 'DEVELOPMENT' in os.environ:
