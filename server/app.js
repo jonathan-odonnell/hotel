96cbda8ed6472311e38f33398a096052049c10fa
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2026-08-28 16:59:55 +0100

    Added database connection and get all hotels api route

--- server/app.js
+++ server/app.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const app = express();
-const path = require('path')
-const cors = require('cors')
+const path = require('path');
+const cors = require('cors');
 const port = 3000;
+const fs = require('fs');
+const pg = require('pg');
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
-
 app.use(cors())
+app.use(express.json())
+
+const db = new pg.Client ({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync("./ca.pem").toString(),
+  },
+});
+
+db.connect().catch(err => {
+  console.error('DB connection error:', err);
+});
+
+app.get('/api/hotels', async (req, res) => {
+    try {
+        const results = await db.query('select * from hotels')
+        res.status(200).json({
+            hotels: results.rows
+        })
+    } catch (err) {
+        res.status(500).json({
+            error: err
+        });
+    }
+})
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
