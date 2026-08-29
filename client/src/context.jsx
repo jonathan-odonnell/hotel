ffc99345a9973bb47785c48412ac7db5dc789c84
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2026-08-29 20:59:26 +0100

    Added filter logic in context

--- client/src/context.jsx
+++ client/src/context.jsx
@@ -4,12 +4,13 @@ import axios from "axios"
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component{
+  // state initial values
   state = {
     rooms: [],
     sortedRooms: [],
     featuredRooms: [],
     loading: true,
-    //
+    sort: "none",
     type: "all",
     capacity: 1,
     price: 0,
@@ -22,6 +23,7 @@ class RoomProvider extends Component{
     error: null
   };
   async componentDidMount() {
+    // get rooms from api and set states
       try {
         let response = await axios.get('/api/rooms')
     
@@ -30,7 +32,9 @@ class RoomProvider extends Component{
         let featuredRooms = rooms.filter(room => room.featured === true);
 
         let maxPrice = Math.max(...rooms.map(item => item.price));
+
         let maxSize = Math.max(...rooms.map(item => item.size));
+        
         this.setState({
           rooms,
           featuredRooms,
@@ -44,26 +48,41 @@ class RoomProvider extends Component{
       } catch (err) {
         this.setState({
           error: err
-        })
-      }
-    }
+        });
+      };
+    };
+  
+    // update sort
+    updateSort = sort => {
+      this.setState({
+        sort
+      });
+    };
+  
+    // get room
     getRoom = slug => {
         let tempRooms = [...this.state.rooms];
         const room = tempRooms.find(room => room.slug === slug);
         return room;
     };
+  
+    // add room
     addRoom = room => {
       this.setState(prev => ({
         rooms: [...prev.rooms, room]
-      }))
+      }));
     };
+  
+    // update room
     updateRoom = updatedRoom => {
       this.setState(prev => ({
         rooms: prev.rooms.map(room =>
           room.id === updatedRoom.id ? updatedRoom : room
         )
-      }))
+      }));
     };
+  
+    // delete room
     deleteRoom = id => {
       this.setState(
         prev => ({
@@ -72,15 +91,21 @@ class RoomProvider extends Component{
           })
         })
       );
-    }
+    };
+
+    // update error state
     updateError = value => {
       this.setState({
         error: value
-      })
+      });
     };
+
     handleChange = event => {
+        // set states and declare variables 
         const target = event.target;
+
         const value =  target.type === "checkbox" ? target.checked : target.value;
+        
         const name = target.name;
     
         this.setState(
@@ -90,6 +115,7 @@ class RoomProvider extends Component{
           this.filterRooms
         );
       };
+
       filterRooms = () => {
         let {
           rooms,
@@ -103,32 +129,75 @@ class RoomProvider extends Component{
         } = this.state;
     
         let tempRooms = [...rooms];
-        // transform values
-        // get capacity
+
+        // transform capacity and price values
         capacity = parseInt(capacity);
         price = parseInt(price);
+
         // filter by type
         if (type !== "all") {
           tempRooms = tempRooms.filter(room => room.type === type);
         }
+
         // filter by capacity
         if (capacity !== 1) {
           tempRooms = tempRooms.filter(room => room.capacity >= capacity);
         }
+        
         // filter by price
         tempRooms = tempRooms.filter(room => room.price <= price);
-        //filter by size
+        
+        // filter by size
         tempRooms = tempRooms.filter(
           room => room.size >= minSize && room.size <= maxSize
         );
-        //filter by breakfast
+        
+        // filter by breakfast
         if (breakfast) {
           tempRooms = tempRooms.filter(room => room.breakfast === true);
         }
-        //filter by pets
+        
+        // filter by pets
         if (pets) {
           tempRooms = tempRooms.filter(room => room.pets === true);
         }
+
+        // sort by default
+        if (sort === "all") {
+          tempRooms = tempRooms.sort(a, b => {
+            return a.id - b.id
+          })
+        }
+
+        // sort by name asc
+        if (sort = "name_asc") {
+          tempRooms = tempRooms.sort(a, b => {
+            return a.name.localeCompare(b.name)
+          })
+        }
+
+        // sort by name desc
+        if (sort = "name_desc") {
+          tempRooms = tempRooms.sort(a, b => {
+            return b.name.localeCompare(a.name)
+          })
+        }
+
+        // sort by price asc
+        if (sort = "price_asc") {
+          tempRooms = tempRooms.sort(a, b => {
+            return a.price - b.price
+          })
+        }
+
+        // sort by price desc
+        if (sort = "price_desc") {
+          tempRooms = tempRooms.sort(a, b => {
+            return b.price - a.price
+          })
+        }
+
+        // set sorted rooms 
         this.setState({
           sortedRooms: tempRooms
         });
