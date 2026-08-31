1d5d46c2d868c4928a49e618641ee3955d2c9c0e
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2026-08-31 14:21:53 +0100

    Added jasmine unit tests

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -2,51 +2,125 @@ const Jasmine = require('jasmine');
 const runner = new Jasmine();
 const request = require('supertest');
 const app = require('../server/app');
+const db = require('../server/db/db');
 
 
-describe("Hotels data API tests", () => {
-  it("API returns a HTTP status of 200 and data for 13 rooms including single economy",
+describe("Rooms data API tests", () => {
+  it("API get rooms returns a HTTP status of 200 and data for 5 rooms",
     async () => {
-        const res = await request(app).get('/hotels/');
+        const fakeRooms = [
+            { name: 'single basic' },
+            { name: 'single economy' },
+            { name: 'double deluxe' },
+            { name: 'family standard' },
+            { name: 'presidential' }
+        ];
+        spyOn(db, 'query').and.returnValue(Promise.resolve({ rows: fakeRooms }));
+        const res = await request(app).get('/api/rooms');
         expect(res.status).toBe(200);
-        expect(res.body.hotels).toBeDefined();
-        expect(Array.isArray(res.body.hotels).toBeTrue());
-        expect(length(res.hotels.body).toBe(13));   
-        expect(res.body.hotels[0].name).toBe("single economy");
+        expect(res.body.rooms).toBeDefined();
+        expect(Array.isArray(res.body.rooms)).toBeTrue();
+        expect(res.body.rooms.length).toBe(5);   
+        expect(res.body.rooms[0].name).toBe('single basic');
+  });
+  it("API add room view returns a HTTP status of 201 and room data",
+    async () => {
+        const fakeRooms = [
+            { name: 'double deluxe' },
+        ];
+        spyOn(db, 'query').and.returnValue(Promise.resolve({ rows: fakeRooms }));
+        const res = await request(app)
+            .post('/api/rooms')
+            .field('name', 'double deluxe')
+            .field('type', 'double')
+            .field('price', 400)
+            .field('size', 500)
+            .field('capacity', 2)
+            .field('pets', false)
+            .field('breakfast', true)
+            .field('featured', false)
+            .field('description', 'test description')
+            .field('extras', JSON.stringify(['an extra']))
+            ;
+        expect(res.status).toBe(201);
+        expect(res.body.room).toBeDefined();
+        expect(
+            res.body.room !== null 
+            && typeof(res.body.room) === 'object' 
+            && !Array.isArray(res.body.room)
+        ).toBeTrue();
+        expect(res.body.room.name).toBe('double deluxe');
+  });
+  it("API update room view returns a HTTP status of 200 and room data",
+    async () => {
+        const fakeRooms = [
+            { name: 'presidential' },
+        ];
+        spyOn(db, 'query').and.returnValue(Promise.resolve({ rows: fakeRooms }));
+        const res = await request(app)
+            .put('/api/rooms/1')
+            .field('name', 'presidential')
+            .field('type', 'presidential')
+            .field('price', 600)
+            .field('size', 1000)
+            .field('capacity', 10)
+            .field('pets', false)
+            .field('breakfast', true)
+            .field('featured', true)
+            .field('description', 'test description')
+            .field('extras', JSON.stringify(['an extra']))
+            ;
+        expect(res.status).toBe(200);
+        expect(res.body.room).toBeDefined();
+        expect(typeof(res.body.room)).toBe('object');  
+        expect(res.body.room.name).toBe('presidential');
+  });
+  it("API delete room view returns a HTTP status of 204 and no data",
+    async () => {
+        spyOn(db, 'query').and.returnValue(Promise.resolve({ rows: 1 }));
+        const res = await request(app).delete('/api/rooms/1');
+        expect(res.status).toBe(204);
+        expect(res.body.rooms).not.toBeDefined();
   });
 });
 
 describe("React app tests", () => {
-    it("Home page returns status 200 and contains the presidential featured room HTML", 
+    it("Home page returns status 200 and contains HTML", 
         async () => {
             const res = await request(app).get('/');
             expect(res.status).toBe(200);
-            expect(res.text).toContain("<!DOCTYPE html");
-            expect(res.text).toContain("<p className=\"room-info\">presidential</p>")
-            expect(res.text).not.toContain("<h4>rooms data loading....</h4>")
-    })
-    it("Rooms page returns status 200 and contains the family standard room HTML", 
+            expect(res.text).toContain('<html lang="en">');
+    });
+    
+    it("Rooms page returns status 200 and contains HTML", 
         async () => {
-            const res = await request(app).get('/');
+            const res = await request(app).get('/rooms');
             expect(res.status).toBe(200);
-            expect(res.text).toContain("<!DOCTYPE html");
-            expect(res.text).toContain("<p className=\"room-info\">family standard</p>")
-            expect(res.text).not.toContain("<h4>rooms data loading....</h4>")
+            expect(res.text).toContain('<html lang="en">');
     })
-    it("Single rooms page returns status 200 and contains the double deluxe HTML", 
+    it("Single rooms page returns status 200 and contains HTML", 
         async () => {
             const res = await request(app).get('/rooms/double-deluxe');
             expect(res.status).toBe(200);
-            expect(res.text).toContain("<!DOCTYPE html");
-            expect(res.text).toContain("<h1>double deluxe room</h1>")
-            expect(res.text).not.toContain("<h3> no such room could be found...</h3>")
+            expect(res.text).toContain('<html lang="en">');
+    })
+    it("Add room page returns status 200 and contains HTML", 
+        async () => {
+            const res = await request(app).get('/rooms/add');
+            expect(res.status).toBe(200);
+            expect(res.text).toContain('<html lang="en">');
+    })
+    it("Update room page returns status 200 and contains HTML", 
+        async () => {
+            const res = await request(app).get('/rooms/double-deluxe/update');
+            expect(res.status).toBe(200);
+            expect(res.text).toContain('<html lang="en">');
     })
     it("Error page returns status 200 and contains the error HTML",
         async () => {
             const res = await request(app).get('/error');
             expect(res.status).toBe(200);
-            expect(res.text).toContain("<!DOCTYPE html");
-            expect(res.text).toContain("<h1>404</h1>")
+            expect(res.text).toContain('<html lang="en">');
     })
 })
 
