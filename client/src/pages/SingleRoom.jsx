e34f6287897177fcc3aafb03973db71b76862a1c
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2026-09-01 22:53:31 +0100

    Amended single room heading styling

--- client/src/pages/SingleRoom.jsx
+++ client/src/pages/SingleRoom.jsx
@@ -101,24 +101,24 @@ export default function SingleRoom () {
         </div>
         <div className="single-room-info">
           <article className="desc">
-            <h3>details</h3>
+            <h2>details</h2>
             <p>{description}</p>
           </article>
           <article className="info">
-            <h3>info</h3>
-            <h6>price : £{price}</h6>
-            <h6>size : {size} SQFT</h6>
-            <h6>
-              max capacity :
+            <h2>info</h2>
+            <h3>price : £{price}</h3>
+            <h3>size : {size} SQFT</h3>
+            <h3>
+              max capacity : 
               {capacity > 1 ? `${capacity} people` : `${capacity} person`}
-            </h6>
-            <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
-            <h6>{breakfast && "free breakfast included"}</h6>
+            </h3>
+            <h3>{pets ? "pets allowed" : "no pets allowed"}</h3>
+            <h3>{breakfast && "free breakfast included"}</h3>
           </article>
         </div>
       </section>
       <section className="room-extras">
-        <h6>extras </h6>
+        <h3>extras </h3>
         <ul className="extras">
           {extras.map((item, index) => (
             <li key={index}>- {item}</li>
